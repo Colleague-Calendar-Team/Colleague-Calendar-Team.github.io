1fae5cbf2d5789b07c637e4e020fb8e14448d35b
ilya.nyrkov <ilya.nyrkov@gmail.com>
2022-04-01 21:39:30 +0300

    changed url in swagger-init

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "calendar_swagger.yaml",
+    url: "https://colleague-calendar-team.github.io/#/",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
